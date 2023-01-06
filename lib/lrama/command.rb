6b7c93bbc7b0040846a84edc08d01b8a1d309410
yui-knk <spiketeika@gmail.com>
2023-01-07 00:07:37 +0900

    Support error recovery
    However `./miniruby -e 'if 1 end'` goes into
    infinite loop...

--- lib/lrama/command.rb
+++ lib/lrama/command.rb
@@ -85,6 +85,7 @@ module Lrama
           header_file_path: header_file,
           context: context,
           grammar: grammar,
+          error_recovery: error_recovery,
         ).render
       end
     end
