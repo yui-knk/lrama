ff03b44c445d00a53d2ac65c9e1c366524e2d1e4
yui-knk <spiketeika@gmail.com>
2023-12-07 00:00:52 +0900

    wip

--- template/lrama/array.c
+++ template/lrama/array.c
@@ -1,118 +1,118 @@
-#define LR_ARRAY_DEFAULT_SIZE 16
+#define YY_ARRAY_DEFAULT_SIZE 16
 
-#ifndef LR_ASSERT
-# define LR_ASSERT(expr) ((void) expr)
-#endif
-
-typedef struct lr_array_struct {
+typedef struct yy_array_struct {
     /* Number of node of the array. */
     long len;
     /* Capacity of the array */
     long capa;
     /* Pointer to the C array */
     void *ary[];
-} lr_array_t;
+} yy_array_t;
 
-static void
-lr_array_heap_alloc(lr_array_t *ary, size_t capa)
+static void *
+yy_array_ary_alloc(size_t capa)
 {
+    void *ptr;
 
+    if (capa == 0) {
+        return NULL;
+    }
+
+    ptr = YYMALLOC(sizeof(void *) * capa);
+    if (!ptr) {
+
+    }
+
+    return ptr;
 }
 
 static void
-lr_array_heap_realloc(lr_array_t *ary, size_t capa)
+yy_array_ary_realloc(yy_array_t *ary, size_t capa)
 {
-    
+    void *ptr = realloc(ary->ary, sizeof(void *) * capa);
+    if (!ptr) {
+
+    }
 }
 
 static void
-lr_array_heap_free(lr_array_t *ary)
+yy_array_ary_free(yy_array_t *ary)
 {
-    
+    YYFREE(ary->ary);
 }
 
 static void
-lr_array_resize_capa(lr_array_t *ary, long capacity)
+yy_array_resize_capa(yy_array_t *ary, long capacity)
 {
-    LR_ASSERT(ary->len <= capacity);
+    YY_ASSERT(ary->len <= capacity);
 
     if (ary->len == 0) {
-        lr_array_heap_alloc(ary, capacity);
+        ary->ary = yy_array_ary_alloc(capacity);
     }
     else {
-        lr_array_heap_realloc(ary, capacity);
+        yy_array_ary_realloc(ary, capacity);
     }
     ary->capa = capacity;
 }
 
 static void
-lr_array_double_capa(lr_array_t *ary, long min)
+yy_array_double_capa(yy_array_t *ary, long min)
 {
     long new_capa = min;
 
-    if (new_capa < LR_ARRAY_DEFAULT_SIZE) {
-        new_capa = LR_ARRAY_DEFAULT_SIZE;
+    if (new_capa < YY_ARRAY_DEFAULT_SIZE) {
+        new_capa = YY_ARRAY_DEFAULT_SIZE;
     }
     new_capa += min;
-    lr_array_resize_capa(ary, new_capa);
+    yy_array_resize_capa(ary, new_capa);
 }
 
-static lr_array_t *
-lr_array_ensure_room_for_push(lr_array_t *ary, long add_len)
+static yy_array_t *
+yy_array_ensure_room_for_push(yy_array_t *ary, long add_len)
 {
     long old_len = ary->len;
     long new_len = old_len + add_len;
 
     if (new_len > ary->capa) {
-        lr_array_double_capa(ary, new_len);
+        yy_array_double_capa(ary, new_len);
     }
 
     return ary;
 }
 
-static lr_array_t *
-lr_array_new_capa(long capa)
+static yy_array_t *
+yy_array_new_capa(long capa)
 {
-    lr_array_t *ary;
+    yy_array_t *ary;
 
-    LR_ASSERT(capa >= 0);
+    YY_ASSERT(capa >= 0);
 
-    ary = ruby_xmalloc(sizeof(lr_array_t));
+    ary = YYMALLOC(sizeof(yy_array_t));
     ary->capa = capa;
-    // alloc
     ary->len = 0;
+    ary->ary = yy_array_ary_alloc(capa);
 
     return ary;
 }
 
-static lr_array_t *
-lr_array_push(lr_array_t *ary, NODE *node)
+static yy_array_t *
+yy_array_push(yy_array_t *ary, NODE *node)
 {
-    lr_array_ensure_room_for_push(ary, 1);
+    yy_array_ensure_room_for_push(ary, 1);
     ary->ary[ary->len++] = node;
 
     return ary;
 }
 
-static void *
-lr_array_pop(lr_array_t *ary)
+static yy_array_t *
+yy_array_new(void)
 {
-    long len = ary->len;
-
-    if (len == 0) return 0;
-    ary->len--;
-
-    return ary->ary[len];
-}
-
-static lr_array_t *
-lr_array_new(void)
-{
-    return lr_array_new_capa(0);
+    return yy_array_new_capa(0);
 }
 
 void
-lr_array_free(lr_array_t *ary)
+yy_array_free(yy_array_t *ary)
 {
-    // lr_array_heap_free
+    yy_array_ary_free(ary);
+    YYFREE(ary);
 }
